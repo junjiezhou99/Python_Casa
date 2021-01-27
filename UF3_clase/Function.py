47d2e004d59c640d3fe601c7817a4630cc3696ea
Admin <jzhou@institutmvm.cat>
2021-01-27 02:19:18 +0100

    ejercicio 3

--- UF3_clase/Function.py
+++ UF3_clase/Function.py
@@ -11,3 +11,28 @@ def file_add():
     str = input("introdueix el text que vols afegir: ")
     f.write(str)
     f.close()
+
+#Opcion de menu
+def file_menu():
+    print("1. Crear un fitxer")
+    print("2. Mostrar el contingut del fitxer")
+    print("3. Modificar el contingut del fitxer")
+    print("4. sortir")
+    menu=int(input("Que vols fer?: "))
+    if (menu==1):
+        name=input("Introdueix el nom del fitxer nou: ")
+        f = open(name, "a+")
+        f.close()
+    elif (menu==2):
+        name=input("Introdueix el nom del fitxer que vols llegir: ")
+        f = open(name, "r")
+        print(f.read())
+        f.close()
+    elif (menu==3):
+        name = input("Introdueix el nom del fitxer que vols modificar: ")
+        text = input("Introdueix el text que vols introduir: ")
+        f = open(name, "w")
+        f.write(text)
+        f.close()
+    else:
+        print("Adios!!!")
--- UF3_clase/UF3_Activitats.py
+++ UF3_clase/UF3_Activitats.py
@@ -1,8 +1,9 @@
 import Function as fc
 
 def main():
-    fc.file_write()
-    fc.file_add()
+    #fc.file_write() #Actividad 1
+    #fc.file_add() #Actividad 2
+    #fc.file_menu() #Actividad 3
 
 if __name__ == '__main__':
     main()
